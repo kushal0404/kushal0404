202f4b984cab4731249bf477411012892df64b47
Gurjyot Singh <singh.gurjyot08@gmail.com>
2022-02-14 17:24:10 -0500

    refactored

--- route.cjs
+++ route.cjs
@@ -1,26 +1,25 @@
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
 const bodyParser = require("body-parser");
 const web3 = require("@solana/web3.js");
 const bs58 = require('bs58');
 const crypto = require('crypto');
 
+var mongo = require('./src/js/mongo.cjs');
+
 const algorithm = 'aes-256-ctr';
 const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzkz9';
 const iv = crypto.randomBytes(16);
-var url = "mongodb+srv://admin:admin@cluster0.fozkz.mongodb.net/Inherit?retryWrites=true&w=majority";
 
 var app = express();
 const port = 3000;
 
+//Registration Page
 app.get('/',function(req,res)
 {
-  console.log("------------------------------------"+__dirname);
-  res.sendFile(__dirname + '/CreateWallet.html');
+  res.sendFile(__dirname + '/src/html/CreateWallet.html');
 });
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(express.static(__dirname));
 
 app.post("/display", (req, res) => {
@@ -75,63 +74,24 @@ app.post("/display", (req, res) => {
 
 })();
 
+async function databaseOperations(name, surname, email, account_type, firstPublicKey, encrypted_sec_key, db_name, db_mail, db_surname) {
+    const result = await mongo.insertAccount({name: name, surname: surname, email: email, account_type: account_type, publicKey: firstPublicKey, secret_key: encrypted_sec_key});
 
-function databaseOperations(name, surname, email, account_type, firstPublicKey, encrypted_sec_key, db_name, db_mail, db_surname) {
-  MongoClient.connect(url, function (err, db) {
-    if (err)
-      throw err;
-    var dbo = db.db("Inherit");
-    insertAccount(name, surname, email, account_type, firstPublicKey, encrypted_sec_key, dbo);
-
-    ({ db_name, db_mail, db_surname } = findAccount(email, dbo, db_name, db_mail, db_surname, db).then(() => {
-    })
-  );
-})};
-
-async function findAccount(email, dbo, db_name, db_mail, db_surname, db){
-    let new_db_name, new_db_mail, new_db_surname
-
-    var query = { email: email };
-
-    const result = dbo.collection("account").find(query);
-
-    await result.forEach(document => {
-      console.log("---------------------email ---------------------" + document.email);
-      new_db_name = document.name;
-      new_db_mail = document.email;
-      new_db_surname = document.surname;
-    });
-    console.log("IN DB NAME: ", new_db_name);
-    console.log("\nIN DB MAIL: ", new_db_mail);
-    console.log("\nIN DB SURNAME: ", new_db_surname);
-
-    return {new_db_name, new_db_mail, new_db_surname};
-}
-
-
-function insertAccount(name, surname, email, account_type, firstPublicKey, encrypted_sec_key, dbo) {
-  var myobj = { name: name, surname: surname, email: email, account_type: account_type, publicKey: firstPublicKey, secret_key: encrypted_sec_key };
-  dbo.collection("account").insertOne(myobj, function (err, res) {
-    if (err)
-      throw err;
-    console.log("1 document inserted");
-  });
-}
+    const findRes = await mongo.findAccount({email: email})
+    console.log("FOUND: ", findRes);
+    db_name = findRes.name;
+    db_surname = findRes.surname;
+    db_mail = findRes.email;
+};
 
 databaseOperations(name, surname, email, account_type, firstPublicKey, encrypted_sec_key, db_name, db_mail, db_surname);
-  console.log("db_mail:"+db_mail);
-  console.log("db_name:"+db_name);
-  console.log("db_surname:"+db_surname);
   let finalString = printData(db_mail, db_name, db_surname, db_pubkey, db_seckey, seckey_hex, seckey_base, encrypted_sec_key, decrypted_sec_key, dec_seckey_hex, originalArray, airDropSignature, firstPublicKey, secondPublicKey, transferSignature);
   res.send(finalString);
 });
 
 const encrypt = (text) => {
-
   const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-
   const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-
   return {
       iv: iv.toString('hex'),
       content: encrypted.toString('hex')
@@ -139,17 +99,23 @@ const encrypt = (text) => {
 };
 
 const decrypt = (hash) => {
-
   const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
-
   const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
-
   return decrpyted.toString();
 };
 
 
 function printData(db_mail, db_name, db_surname, db_pubkey, db_seckey, seckey_hex, seckey_base, encrypted_sec_key, decrypted_sec_key, dec_seckey_hex, originalArray, airDropSignature, firstPublicKey, secondPublicKey, transferSignature) {
   let finalString = "<h1>Account Created Successfully!</h1>";
+  finalString += "</br>";
+  finalString += "Name in DB";
+  finalString += db_name;
+  finalString += "</br>";
+  finalString += "Surname in DB";
+  finalString += db_surname;
+  finalString += "</br>";
+  finalString += "Email in DB";
+  finalString += db_mail;
   finalString += "</br>";
   finalString += "Secret Key in HEX";
   finalString += "</br>";
@@ -188,8 +154,6 @@ function printData(db_mail, db_name, db_surname, db_pubkey, db_seckey, seckey_he
   finalString += transferSignature;
   return finalString;
 }
-
-
 
 function encryptions(seckey_base) {
   let encrypted_sec_key = encrypt(seckey_base);
